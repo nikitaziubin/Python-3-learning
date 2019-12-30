8b26f9806782bb0f4cd5821b82824c7de28276eb
nikitaziubin <nikita.ziubin@gmail.com>
2019-12-30 19:41:37 +0000

    Added some comments. Improved formatting.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 for i in '1=2kokk':  
    print (i + '1', '\n')
+   
+# test python maps
 d ={'test' : 45, 'str' :'H'}
 print(d['test'])
+
+#Test python lists.
 d=[4, 2, 77]
 print(d[-1])
 for i in d:  
    print (i + 1, '\n')
+   
+#Learn for loops over list.
 for i in d:  
    print (i + 1, '\n')
+
+#Learn python range function 
 a = range(5, 10)
 print(a) # range(5, 10)
  
@@ -15,7 +23,8 @@ b = list(range(1, 10, 2))
 print(b) # [1, 3, 5, 7, 9]1
 for number in range(10):
     print(number)
-    
+
+#Изучение условного цикла в питоне.     
 print("АРЕФМЕТИЧИ")
 i = 1
 while i < 1000000000000000000:
