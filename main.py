3c708624dddb5ca7d875cd1a32e7c4113a430c56
nikitaziubin <nikita.ziubin@gmail.com>
2019-12-30 12:45:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+for i in '1=2kokk':  
+   print (i + '1', '\n')
+d ={'test' : 45, 'str' :'H'}
+print(d['test'])
+d=[4, 2, 77]
+print(d[-1])
+for i in d:  
+   print (i + 1, '\n')
+for i in d:  
+   print (i + 1, '\n')
+a = range(5, 10)
+print(a) # range(5, 10)
+ 
+b = list(range(1, 10, 2))
+print(b) # [1, 3, 5, 7, 9]1
+for number in range(10):
+    print(number)
+print("АРЕФМЕТИЧИ")
+i = 1
+while i < 1000000000000000000:
+    print(i)
+    i = i * 2
